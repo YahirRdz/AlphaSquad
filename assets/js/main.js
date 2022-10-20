c7015144bd98dbddb08e5f5820acbb00a6a89618
Yahir <yajosh03@gmail.com>
2022-10-20 20:32:16 +0500

    Transitions

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,23 @@ const sr = ScrollReveal({
   reset: true,
 });
 
-sr.reveal(".home__boxes", {
-  interval: 200,
+sr.reveal(
+  ".box__one,.box__two,.box__three,.rectangule,.circle,.img_product,.opinion__cards ",
+  {
+    interval: 200,
+  }
+);
+
+const srD = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2000,
+  reset: true,
 });
+
+srD.reveal(
+  ".nav,.home__data,.product__info,.pricing,.opinion__head,.contact__container,.footer__sec1,.footer__sec2,.footer__sec3,.footer__sec4",
+  {
+    interval: 200,
+  }
+);
